d85b6924f42384209997eef06ec3559ea98e4f33
erikbv99421 <erikbv99421@gmail.com>
2016-02-24 23:22:04 +0000

    arreglado el conversor

--- temperature.js
+++ temperature.js
@@ -1,26 +1,28 @@
 "use strict"; // Use ECMAScript 5 strict mode in browsers that support it
-function calculate() {
+
+function calcular() {
   var result;
+  var original = document.getElementById("original");
   var temp = original.value;
-  var regexp = /^\s*([-+]?\d+(?:\.\d*)?(?:e[+-]?\d+(?:\.\d*)?)?)\s*(f(?:a(?:r(?:e(?:n(?:h(?:e(?:i(?:t)?)?)?)?)?)?)?)?|c(?:e(?:l(?:s(?:i(?:u(?:s)?)?)?)?)?)?)\s*$/i;
+  var regexp = /([-+]?\d+(?:\.\d*)?)\s*([fFcC])/;
+  var mejorRegex = /(^[-+]?[0-9]*\.?[0-9]+(?:[eE][-+]?[0-9]+)?)\s*([fFcC])/
+  var x = temp.match(mejorRegex);
 
-  var m = temp.match(regexp);
-
-  if (m) {
-    var num = m[1];
-    var type = m[2];
+  if (x) {
+    var num = x[1];
+    var type = x[2];
     num = parseFloat(num);
     if (type == 'c' || type == 'C') {
       result = (num * 9/5)+32;
-      result = result.toFixed(1)+" Farenheit"
+      result = result.toFixed(1)+" Farenheit";
     }
     else {
       result = (num - 32)*5/9;
-      result = result.toFixed(1)+" Celsius"
+      result = result.toFixed(1)+" Celsius";
     }
     converted.innerHTML = result;
   }
   else {
-    converted.innerHTML = "ERROR! Try something like '-4.2C' instead";
+    converted.innerHTML = "¡ERROR! Intente con valores correctos [-,+] [Número] [Medida] e.g: '-4.2C' ";
   }
 }
